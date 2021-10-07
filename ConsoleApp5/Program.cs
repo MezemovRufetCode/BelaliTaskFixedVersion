5482b406c43a910c8c90788ffd594e12a68a0be4
MezemovRufetCode <rufatam@code.edu.az>
2021-10-07 09:25:08 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Include main string:");
+            string mstring = Console.ReadLine();
+            Console.WriteLine("Include a text which is u are looking for:");
+            string lstring = Console.ReadLine();
+            Console.WriteLine(mstring.MyContains(lstring));          
+        }
+    }
+    static class Extension
+    {
+        public static bool MyContains(this string mainstr,string part)
+        {
+            int temp = 0;
+            for (int i = 0; i < mainstr.Length;i++ )
+            {
+                if (mainstr[i] == part[0])
+                {
+                    for(int k = i + 1, j = 1; j < part.Length; j++, k++)
+                    {
+                        if (mainstr[k] == part[j])
+                        {
+                            temp++;
+                        }
+                    }
+                }
+            }
+            if (temp == part.Length - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
